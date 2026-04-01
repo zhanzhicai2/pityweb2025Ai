239ccbd13efc286c789f00ddd0ab08724b755059
zhanzhicai2 <1610893869@qq.com>
2026-04-01 17:13:10 +0800

    chore: 更新配置,改成：wssUrl: 'ws://127.0.0.1:7777',

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -23,8 +23,10 @@ const Settings: LayoutSettings & {
   pwa: false,
   logo: '/logo.svg',
   iconfontUrl: '//at.alicdn.com/t/font_915840_kom9s5w2t6k.js',
-  apiUrl: 'localhost:8000',
-  wssUrl: 'ws://localhost:8000',
+  // apiUrl: 'localhost:8000',
+  // wssUrl: 'ws://localhost:8000',
+  apiUrl: '127.0.0.1:7777',
+  wssUrl: 'ws://127.0.0.1:7777',
   https: false,
   backend: true,
 };
